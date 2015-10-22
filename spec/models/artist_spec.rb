a61e806bb3e0bb0709fb992042430f6c5801ff24
Teddy Caddy <info@teddycaddy.com>
2015-10-22 14:42:02 -0400

    unit test for Artist

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -4,5 +4,10 @@ RSpec.describe Artist, type: :model do
   it "has a factory for testing" do
     expect(build(:artist)).to be_valid
   end
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:name) }
+
+  let(:artist) { build(:artist) }
+  describe "ActiveModel validations" do
+    it { expect(artist).to validate_presence_of(:name) }
+  end
 end
