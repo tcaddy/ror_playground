e6a2e3b91be5147c7140cd7f01187ef309b157e4
Teddy Caddy <info@teddycaddy.com>
2015-10-23 09:39:57 -0400

    add custom validation: AlbumYearValidator

--- lib/validations.rb
+++ lib/validations.rb
@@ -1,3 +1,4 @@
+# validates a URL
 require 'uri'
 class URLValidator < ActiveModel::Validator
   def validate(record)
@@ -14,6 +15,20 @@ class URLValidator < ActiveModel::Validator
         end
         if err
           record.errors.add(field, 'invalid URL format')
+        end
+      end
+    end
+  end
+end
+
+# validates a suitable year for an album, allowing nil values
+# (:validates_numericality_of doesn't like nil values)
+class AlbumYearValidator < ActiveModel::Validator
+  def validate(record)
+    options[:fields].each do |field|
+      if year = record.send(field)
+        unless year.kind_of?(Fixnum) or year<1900 or year>Date.today.year+1
+          record.errors.add(attr, 'is not a valid year')
         end
       end
     end
