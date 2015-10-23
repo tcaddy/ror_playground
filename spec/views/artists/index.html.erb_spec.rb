b4a3ca838b21933b80f8053abfa3f0ff934c7e69
Teddy Caddy <info@teddycaddy.com>
2015-10-23 14:23:28 -0400

    improve flow b/t Artist and Album in views

--- spec/views/artists/index.html.erb_spec.rb
+++ spec/views/artists/index.html.erb_spec.rb
@@ -2,18 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "artists/index", type: :view do
   before(:each) do
-    assign(:artists, [
-      Artist.create!(
-        :name => "Name"
-      ),
-      Artist.create!(
-        :name => "Name"
-      )
-    ])
+    @artists = [
+      create(:artist),
+      create(:artist)
+    ]
   end
 
   it "renders a list of artists" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    @artists.each do |artist|
+      assert_select "tr[data-row-id='#{artist.id}'] td", text: artist.name, count:1
+    end
   end
 end
--- spec/views/artists/show.html.erb_spec.rb
+++ spec/views/artists/show.html.erb_spec.rb
@@ -2,13 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "artists/show", type: :view do
   before(:each) do
-    @artist = assign(:artist, Artist.create!(
-      :name => "Name"
-    ))
+    @artist = assign(:artist, create(:artist))
+    2.times do
+      create(:album,artist_id:@artist.id)
+    end
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/#{@artist.name}/)
+
+    assert_select "ul.artist-albums" do
+      @artist.albums.each do |album|
+        assert_select "a[href='#{url_for(album)}']", text: album.name
+      end
+    end
   end
 end
