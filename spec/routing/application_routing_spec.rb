24409a0389faefebeb4442b319919da6e004e5ea
Teddy Caddy <info@teddycaddy.com>
2015-10-23 20:02:07 -0400

    route '/' to artists#index

---
+++ spec/routing/application_routing_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe ApplicationController, type: :routing do
+  describe "routing" do
+
+    it "routes to #index" do
+      expect(:get => "/").to route_to("artists#index")
+    end
+
+  end
+end
