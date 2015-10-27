129eb0f51d5ac86fdb0de0a174e3e717fe20d760
Teddy Caddy <info@teddycaddy.com>
2015-10-27 15:52:57 -0400

    switch from sqlite3 to postgres

--- lib/reset_auto_increment.rb
+++ lib/reset_auto_increment.rb
@@ -8,6 +8,10 @@ class ResetAutoIncrement
           sql = "delete from sqlite_sequence where name = '#{table}'"
         when 'mysql'
           sql = "ALTER TABLE `#{table}` auto_increment=1"
+        when 'postgresql'
+          # assume primary key is :id
+          sql = "ALTER SEQUENCE #{table}_id_seq RESTART WITH 1"
+        else raise "Unknown database adapter: #{ActiveRecord::Base.connection.instance_variable_get("@config")[:adapter]}"
       end
       ActiveRecord::Base.connection.execute(sql) unless sql.nil?
     end
