42a71f510f1f2e04ab37a94b774421da7d62f5f7
Teddy Caddy <info@teddycaddy.com>
2015-10-27 21:53:30 -0400

    destroy songs on delete

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,7 @@
 require "#{File.expand_path(::Rails.root)}/lib/validations"
 class Album < ActiveRecord::Base
   belongs_to :artist
-  has_many :songs
+  has_many :songs, dependent: :destroy
 
   validates :name, presence: true
   validates_with URLValidator, fields: [:photo_url]
