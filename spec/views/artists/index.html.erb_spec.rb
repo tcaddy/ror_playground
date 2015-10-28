3a2bd01ce3f552ed8736eb3b76cf6a31ccb66e1d
Teddy Caddy <info@teddycaddy.com>
2015-10-27 22:15:48 -0400

    show a jumbotron on artists#index if @artists.size==0

--- spec/views/artists/index.html.erb_spec.rb
+++ spec/views/artists/index.html.erb_spec.rb
@@ -1,22 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "artists/index", type: :view do
-  before(:each) do
-    @artists = Kaminari.paginate_array([
-      create(:artist),
-      create(:artist)
-    ]).page(1)
-  end
+  context "regular listing" do
+    before(:each) do
+      @artists = Kaminari.paginate_array([
+        create(:artist),
+        create(:artist)
+      ]).page(1)
+    end
 
-  it "renders a list of artists" do
-    render
-    assert_select "div.table-responsive table.table-striped" do
-      @artists.each do |artist|
-        assert_select "tr[data-row-id='#{artist.id}']", count:1 do
-          assert_select "td[data-attr='name'] a[href='#{url_for(artist)}']", text: artist.name
-          assert_select "td a[href='#{url_for(artist)}'][data-method='delete'] span.glyphicon-remove"
+    it "renders a list of artists" do
+      render
+      assert_select "div.table-responsive table.table-striped" do
+        @artists.each do |artist|
+          assert_select "tr[data-row-id='#{artist.id}']", count:1 do
+            assert_select "td[data-attr='name'] a[href='#{url_for(artist)}']", text: artist.name
+            assert_select "td a[href='#{url_for(artist)}'][data-method='delete'] span.glyphicon-remove"
+          end
         end
       end
+    end
+  end
+
+  context "with empty database table" do
+    before(:each) do
+      @artists = Kaminari.paginate_array([]).page(1)
+    end
+    it "renders a jumbotron" do
+      render
+      assert_select ".jumbotron"
+      assert_select "a[href='#{new_artist_path}']"
     end
   end
 end
